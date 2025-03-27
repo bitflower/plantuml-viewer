6daf1c5376ecb058a9cbdda77b456fdebfee932f
Matthias Max <matthias.max@bitflower.net>
2025-03-27 21:45:19 +0100

    .

--- public/script.js
+++ public/script.js
@@ -2,6 +2,8 @@ function renderUML() {
   const input = document.getElementById('umlInput').value;
   const encoded = plantumlEncoder.encode(input);
 
+  console.log('Encoded:', encoded);
+
   const baseUrl = 'http://localhost:8080/plantuml/svg/';
   // const imageUrl = baseUrl + encoded;
   const imageUrl = `/uml/${encoded}`;
